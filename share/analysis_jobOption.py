246c42d68b901a67d5abcff26c30c077198bf9be
siinn <che.32@cadence8.mps.ohio-state.edu>
2017-05-29 13:30:04 -0400

    Updated jobOption

--- share/analysis_jobOption.py
+++ share/analysis_jobOption.py
@@ -9,8 +9,8 @@ import AthenaPoolCnvSvc.ReadAthenaPool
 from AthenaCommon.GlobalFlags import globalflags
 import glob   
 
-theApp.EvtMax = -1
-#theApp.EvtMax = 500
+#theApp.EvtMax = -1
+theApp.EvtMax = 200
 
 #--------------------------------------------
 # DAOD_SUSY15 data
@@ -26,16 +26,22 @@ theApp.EvtMax = -1
 # Signal MC samples
 #--------------------------------------------
 
+# ee test
+#svcMgr.EventSelector.InputCollections = glob.glob('/n/atlas05/userdata/sche/20.7.8.7.SUSY15/output/DAOD_SUSY15.DAOD_SUSY15.ee.pool.root')
+# emu test
+#svcMgr.EventSelector.InputCollections = glob.glob('/n/atlas05/userdata/sche/20.7.8.7.SUSY15/output/DAOD_SUSY15.DAOD_SUSY15.emu.pool.root')
+
 # SUSY15, official
-svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301911.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301912.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301913.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r14_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301914.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301915.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r11_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301916.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301917.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r13_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301918.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r14_EXT0/*.root')
-#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301919.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r13_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301911.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301912.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301913.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m250t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r14_EXT0/*.root')
+svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301914.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301915.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r11_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301916.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m500t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r12_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301917.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t100.recon.DAOD_RPVLL.e4125_s2698_r8788.r13_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301918.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t250.recon.DAOD_RPVLL.e4125_s2698_r8788.r14_EXT0/*.root')
+#svcMgr.EventSelector.InputCollections += glob.glob( '/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/zprimemumu/user.sche.mc15_13TeV.301919.Pythia8EvtGen_A14NNPDF23LO_LLzprimemumu_m1000t500.recon.DAOD_RPVLL.e4125_s2698_r8788.r13_EXT0/*.root')
+
 
 #--------------------------------------------
 # Background samples
@@ -46,6 +52,8 @@ svcMgr.EventSelector.InputCollections = glob.glob( '/n/atlas05/userdata/sche/MC1
 #svcMgr.EventSelector.InputCollections = glob.glob("/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/background/user.sche.mc15_13TeV.361066.Sherpa_CT10_lllvSFPlus.recon.DAOD_SUSY15.e3836_s2608_s2183_r8788.r1_EXT0/*.root")
 #svcMgr.EventSelector.InputCollections = glob.glob("/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/background/user.sche.mc15_13TeV.361068.Sherpa_CT10_llvv.recon.DAOD_SUSY15.e3836_s2608_s2183_r8788.r1_EXT0/*.root")
 #svcMgr.EventSelector.InputCollections = glob.glob("/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/background/user.sche.mc15_13TeV.361063.Sherpa_CT10_llll.recon.DAOD_SUSY15.e3836_s2608_s2183_r8788.r1_EXT0/*.root")
+
+# ttbar
 #svcMgr.EventSelector.InputCollections = glob.glob("/n/atlas05/userdata/sche/MC15/DAOD_SUSY15/background/user.sche.mc15_13TeV.410252.Sherpa_221_NNPDF30NNLO_ttbar_dilepton_MEPS_NLO.recon.DAOD_SUSY15.e5450_s2726_r8788.r1_EXT0/*.root")
 
 
@@ -70,10 +78,12 @@ from DisplacedDimuonAnalysis.DisplacedDimuonAnalysisConf import DisplacedDimuonA
 
 # Data and MC
 algseq += CfgMgr.DisplacedDimuonAnalysisAlg()
+#algseq += CfgMgr.FlipBkgEst()
+#algseq += CfgMgr.SUSYDV()
 #algseq += CfgMgr.MuonPlots()
 
 # MC sample only
-algseq += CfgMgr.DVEfficiency()
+#algseq += CfgMgr.DVEfficiency()
 #algseq += CfgMgr.MuonEfficiency()
 #algseq += CfgMgr.TruthPlots()
 #algseq += CfgMgr.VertexRes()
@@ -84,21 +94,26 @@ algseq += CfgMgr.DVEfficiency()
 #---------------------------------------------------------------
 # including the tools /cuts needed by the algorithm, and configuring them
 #---------------------------------------------------------------
+svcMgr.MessageSvc.OutputLevel = INFO
+#svcMgr.MessageSvc.OutputLevel = DEBUG
+svcMgr.MessageSvc.defaultLimit = 9999
 
 # histogram output service
 if not hasattr(svcMgr, 'THistSvc'): svcMgr += CfgMgr.THistSvc()
 svcMgr.THistSvc.Output += ["DV DATAFILE='output.root' OPT='RECREATE'"]
-svcMgr.MessageSvc.OutputLevel = INFO
-svcMgr.MessageSvc.defaultLimit = 9999
 
 # DVCuts tool
 ToolSvc += CfgMgr.DDL__DVCuts("DiLepBaseCuts")
 ToolSvc.DiLepBaseCuts.distMin  = 2. # mm
 ToolSvc.DiLepBaseCuts.DisabledModuleMapFile = "DisabledModuleMap_Run2_v2.root"
+ToolSvc.DiLepBaseCuts.MaterialMapFile = "materialMap3D_Run2_v2.1.1.root"
 
 # EventCuts
 ToolSvc += CfgMgr.DDL__EventCuts("DiLepEventCuts")
-ToolSvc.DiLepEventCuts.TriggerNames = ["HLT_mu60_0eta105_msonly"]
+ToolSvc.DiLepEventCuts.TriggerNames = [ "HLT_mu60_0eta105_msonly",
+                                        "HLT_g140_loose",
+                                        "HLT_2g50_loose"
+                                        ]
 
 # GoodRunsListSelectorTool
 vecStringGRL = '/n/atlas05/userdata/sche/2.4.21.DV_Analysis/DV_xAODAnalysis/DVAnalyses/data/data16_13TeV.periodAllYear_DetStatus-v83-pro20-15_DQDefects-00-02-04_PHYS_StandardGRL_All_Good_25ns_DAOD_RPVLL_r8669.xml'
@@ -106,14 +121,26 @@ ToolSvc += CfgMgr.GoodRunsListSelectionTool("GRLTool")
 ToolSvc.GRLTool.GoodRunsListVec=[vecStringGRL]
 ToolSvc.GRLTool.OutputLevel = INFO
 
-# Overlap removal tool
-#from AssociationUtils.config import recommended_tools
-#orTool = recommended_tools(masterName="OverlapRemovalTool",
-#                           doElectrons=False,
-#                           doMuons=True,
-#                           doJets=False,
-#                           doTaus=False,
-#                           doPhotons=False)
+# Muon selection tool
+ToolSvc += CfgMgr.CP__MuonSelectionTool("MuonSelectionTool")
+ToolSvc.MuonSelectionTool.MaxEta = 2.7
+ToolSvc.MuonSelectionTool.MuQuality = 2
+ToolSvc.MuonSelectionTool.OutputLevel = INFO
+
+# Muon correction tool
+ToolSvc += CfgMgr.CP__MuonCalibrationAndSmearingTool("MuonCorrectionTool")
+ToolSvc.MuonCorrectionTool.Year = "Data16"
+ToolSvc.MuonCorrectionTool.Release = "Recs2016_15_07"
+ToolSvc.MuonCorrectionTool.SagittaCorr = True
+ToolSvc.MuonCorrectionTool.OutputLevel = INFO
+
+# Electron Likelihood tool
+confDir = "ElectronPhotonSelectorTools/offline/mc15_20160512/"
+ToolSvc += CfgMgr.AsgElectronLikelihoodTool("ElectronLikelihoodTool")
+#ToolSvc.ElectronLikelihoodTool.ConfigFile= confDir+"ElectronLikelihoodLooseOfflineConfig2016_Smooth_NoD0.conf"
+ToolSvc.ElectronLikelihoodTool.WorkingPoint= "MediumLHElectron"
+#ToolSvc.ElectronLikelihoodTool.WorkingPoint= "TightLHElectron"
+ToolSvc.ElectronLikelihoodTool.OutputLevel = INFO
 
 
 #---------------------------------------------------------------
@@ -142,5 +169,12 @@ ToolSvc.GRLTool.OutputLevel = INFO
 #ToolSvc += CfgMgr.LumiBlockMetaDataTool("LumiBlockMetaDataTool")
 #svcMgr.MetaDataSvc.MetaDataTools += [ ToolSvc.LumiBlockMetaDataTool ]
 
-
+# Overlap removal tool
+#from AssociationUtils.config import recommended_tools
+#orTool = recommended_tools(masterName="OverlapRemovalTool",
+#                           doElectrons=False,
+#                           doMuons=True,
+#                           doJets=False,
+#                           doTaus=False,
+#                           doPhotons=False)
 
